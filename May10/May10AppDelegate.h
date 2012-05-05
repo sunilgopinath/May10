5159318a79b47a6b97f2d8851e5a832ba6457042
Sunil Gopinath <sxg282@gmail.com>
2012-05-05 08:54:28 -0400

    actual first commit

---
+++ May10/May10AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  May10AppDelegate.h
+//  May10
+//
+//  Created by Sunil Gopinath on 5/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface May10AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ May10/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  May10
+//
+//  Created by Sunil Gopinath on 5/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "May10AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([May10AppDelegate class]));
+    }
+}
